239856c7a9c753eafaa4274d8cc8ae0e62b71dab
quitegreensky <quitegreensky@gmail.com>
2025-11-10 15:53:13 +0330

    update

--- pyppeteer/__init__.py
+++ pyppeteer/__init__.py
@@ -5,6 +5,8 @@
 
 import logging
 import os
+import platform
+
 
 from appdirs import AppDirs
 
@@ -15,9 +17,24 @@ try:
 except Exception:
     __version__ = None
 
+def get_os_type():
+    os_name = platform.system()
+    if os_name == "Windows":
+        return "win"
+    elif os_name == "Linux":
+        return "linux"
+    elif os_name == "Darwin":
+        return "mac"
+    else:
+        return os_name  # For any other OS
 
 # old chrome version panic upon launching - this one may not match the base puppeteer version, but at least it launches
-__chromium_revision__ = '1000027'
+if get_os_type()=="linux":
+    __chromium_revision__ = "1000000"
+else:
+    __chromium_revision__ = '1000027'
+
+
 __base_puppeteer_version__ = 'v1.6.0'
 __pyppeteer_home__ = os.environ.get('PYPPETEER_HOME', AppDirs('pyppeteer').user_data_dir)  # type: str
 DEBUG = False
